ab1e1afdf08228aa46ee491f78cb2dccd5febaa2
Tulio-Rangel <tjrn20@hotmail.com>
2023-07-19 15:45:01 -0500

    first commit

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+//   var myName = 'Tulio';
+//   late final myName;
+//   String myName = 'Tulio';
+  const myName = 'Tulio';
+    
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
